3376dbe7f73af9545ffecbf066f583f3b83302af
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-05-15 00:14:25 -0400

    finished step 03

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+class Footer extends React.Component {
+
+
+  render() {
+    return (
+      <div className="">
+        <h3>@copy Sarah Teklemariam</h3>
+      </div>
+    );
+  }
+}
+export default Footer;
---
+++ src/Weather.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+
+
+
+class Weather extends React.Component {
+
+
+  render() {
+    return (
+      <div className="">
+      <h3>{this.props.weather[0]}</h3>
+      <h3>{this.props.weather[1]}</h3>
+      <h3>{this.props.weather[2]}</h3>
+      </div>
+    );
+  }
+}
+export default Weather;
